7dec35dafd71ffe3be91c142b89713c7ccfb63a9
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2020-08-18 15:36:02 +0300

    implementing hero class and monster class + basic fighting

--- GameOne/GameOne/Program.cs
+++ GameOne/GameOne/Program.cs
@@ -10,11 +10,103 @@ namespace GameOne
 
         static void Main(string[] args)
         {
-            SettingHeroAndIntro();
+
+            Hero myCharacter = SettingHeroAndIntro();
+
+            int roomNumber = 1;
+            Random rand = new Random();
+            string roomType = string.Empty;
+
+            Monster hellHound = new Monster { HP = 10, DMG = 6 , MonsterName = "Hell Hound"};
+            Monster bear = new Monster { HP = 15, DMG = 4 , MonsterName = "Cave bear"};
+
+            while (true)
+            {
+                if (myCharacter.Health <= 0)
+                {
+                    break;
+                }
+
+                if (rand.Next(1, 10) < 6)
+                {
+                    roomType = "monster room";
+
+                    Console.WriteLine($"You entered room: {roomNumber} and found {roomType}");
+
+                    Monster currMonster = new Monster { HP = 0, DMG = 0 };
+
+                    if(rand.Next(1, 6) < 3)
+                    {
+                        currMonster = hellHound;
+                    }
+                    else
+                    {
+                        currMonster = bear;
+                    }
+
+                    while (currMonster.HP > 0 || myCharacter.Health > 0)
+                    {
+                        currMonster.HP -= myCharacter.Attack;
+                        if (currMonster.HP <= 0)
+                        {
+                            Console.WriteLine($"You have won the battel against {currMonster.MonsterName}");
+                            ResettingMonsterHp(hellHound, bear, currMonster);
+                            break;
+                        }
+                        else
+                        {
+                            System.Threading.Thread.Sleep(1000);
+                            Console.WriteLine($"You attack {currMonster.MonsterName} with your weapon and deal {myCharacter.Attack} Dmg");
+                            System.Threading.Thread.Sleep(1000);
+                            Console.WriteLine($"{currMonster.MonsterName} has {currMonster.HP} HP left");
+                        }
+                        myCharacter.Health -= currMonster.DMG;
+                        if(myCharacter.Health <= 0)
+                        {
+                            System.Threading.Thread.Sleep(1000);
+                            Console.WriteLine($"{currMonster.MonsterName} attack and kills you.");
+                            System.Threading.Thread.Sleep(1000);
+                            Console.WriteLine($"{myCharacter.Name} has lost the battle agains evil");
+                            break;
+                        }
+                        else
+                        {
+                            System.Threading.Thread.Sleep(1000);
+                            Console.WriteLine($"{currMonster.MonsterName} attacks you and deal {currMonster.DMG} dmg to you");
+                            System.Threading.Thread.Sleep(1000);
+                            Console.WriteLine($"You survived the attack, and have {myCharacter.Health} health left");
+                        }
+
+                    }
+                }
+                else
+                {
+                    roomType = "treasure room";
+
+                    Console.WriteLine($"You entered room: {roomNumber} and found {roomType}");
+                }
+
+                roomNumber++;
+                
+            }
+
+            Console.WriteLine("You died!");
 
         }
 
-        private static void SettingHeroAndIntro()
+        private static void ResettingMonsterHp(Monster hellHound, Monster bear, Monster currMonster)
+        {
+            if (currMonster == bear)
+            {
+                currMonster.HP = 15;
+            }
+            else if (currMonster == hellHound)
+            {
+                currMonster.HP = 10;
+            }
+        }
+
+        private static Hero SettingHeroAndIntro()
         {
             Console.WriteLine("Intro");  //put some intro
 
@@ -37,7 +129,10 @@ namespace GameOne
             Console.WriteLine($"Remmenber, those are your stats: {myCharacter.ToString()}");
             System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Okey, grab your boots and let's dive into the dungeon full of nasty creatures");
+            return myCharacter;
         }
+
+
 
         private static Hero ChooseHero()
         {
@@ -63,7 +158,7 @@ namespace GameOne
             return myCharacter;
         }
 
-        
+
     }
 
     public class Hero
@@ -78,7 +173,7 @@ namespace GameOne
         public string Name { get; set; }
 
         public Dictionary<string, List<int>> SpellBook = new Dictionary<string, List<int>>();
-        public  Hero()
+        public Hero()
         {
             this.Attack = Attack;
             this.Deffence = Deffence;
@@ -88,10 +183,31 @@ namespace GameOne
             this.HeroClass = HeroClass;
         }
 
-        public  override string ToString()
+        public override string ToString()
         {
             return $"Health = {Health}, mana = {Mana}, attack = {Attack}, deffence = {Deffence}";
         }
 
     }
+
+
+    public class Monster
+    {
+        public int HP { get; set; }
+        public int DMG { get; set; }
+        public string MonsterName { get; set; }
+
+        public Monster()
+        {
+            this.DMG = DMG;
+            this.HP = HP;
+            this.MonsterName = MonsterName;
+        }
+
+        public override string ToString()
+        {
+            return $"{MonsterName} with Health = {HP}, attack = {DMG}";
+        }
+    }
+
 }
